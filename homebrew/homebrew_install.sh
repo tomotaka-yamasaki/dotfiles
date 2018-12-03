6534c1c459c9b8014d0c349aa466aee86186471e
kuroki-ty <t.yamasaki11@gmail.com>
2018-12-03 20:41:22 +0900

    Add: homebrew

---
+++ homebrew/homebrew_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "installing homebrew..."
+which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "run brew doctor..."
+which brew >/dev/null 2>&1 && brew doctor
+
+echo "run brew update..."
+which brew >/dev/null 2>&1 && brew update
+
+brew bundle
+
+cat << END
+
+**************************************************
+HOMEBREW INSTALLED! bye.
+**************************************************
+
+END
